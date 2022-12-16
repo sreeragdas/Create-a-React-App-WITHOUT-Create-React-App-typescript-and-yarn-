33a96c9e8a0d01879e48159a0ba0b1ac46bfd1bb
EmbarckleSreerag <sreerag.em@embarckle.com>
2022-12-16 16:50:17 +0530

    new changes

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import 'file-saver';
+import 'react-circular-progressbar/dist/styles.css';
+import 'react-notifications/lib/notifications.css';
+import 'semantic-ui-css/semantic.min.css';
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+import { Main } from './src/screens/Main';
+
+
+
+
+
+
+ReactDOM.render(
+   
+     <Main />,
+    document.getElementById('root')
+);
---
+++ src/screens/Main.tsx
@@ -0,0 +1,9 @@
+import './stylescss';
+export const Main: React.FC = () => {
+
+  return (
+    <div className="weather">
+     <div>hi main</div>
+    </div>
+  );
+};
