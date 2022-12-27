397b6c20540d2410df7a56873dd420c6ecf45efb
EmbarckleSreerag <sreerag.em@embarckle.com>
2022-12-27 18:37:02 +0530

    chore(react):router added

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import './App.scss';
 import { About } from "./src/components/about/About";
+import { Contact } from "./src/components/contact/Contact";
 import { Home } from "./src/components/home/home";
 import { Layout } from "./src/components/layout";
+import { Portfolio } from "./src/components/portfolio/Portfolio";
 const App = () => {
 
   return (
@@ -12,6 +14,8 @@ const App = () => {
    <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contact" element={<Contact />} />
           </Route>
    </Routes>
    
