ec87cbb9dd09e42a69a45a0f94f062001cdd3c30
sreeragdas <hsreerag25@gmail.com>
2022-12-28 08:23:19 +0530

    chore(client):style added

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,26 +1,54 @@
 import './index.scss'
 import LogoTitle from '../../assets/images/logo-s.png'
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { AnimatedLetters } from '../animatedLetters/AnimatedLetters';
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { AnimatedLetters } from '../animatedLetters/AnimatedLetters'
 export const Home: React.FC = () => {
-    const [letterClass , setLetterClass]=useState('text-animate');
-    const nameArray=['S' , 'r' , 'e' , 'e' , 'r' , 'a' ,'g']; 
-    const jobArray = ['w','e','b', '','d','e','v','e','l','o','p','e','r','.']
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['S', 'r', 'e', 'e', 'r', 'a', 'g']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    '',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-    return(<div className='cointaner home-page'>
-        <div className='text-zone'>
-            <h1>Hi , <br/> I'm
-         
-            <AnimatedLetters letterClass={letterClass} strArray={nameArray}  idx={15} />
-           
-            <br />
-            web developer
-            </h1>
-            <h2>Frontent developer / Javascript Expert</h2>
-            <Link to="/contact" className='flat-button' >CONTACT ME</Link>
-
-
-        </div>
-    </div>);
+  return (
+    <div className="cointaner home-page">
+      <div className="text-zone">
+        <h1>
+          Hi , <br /> I'm
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          web developer
+        </h1>
+        <h2>Frontent developer / Javascript Expert</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+        <form method="get" action="fileName">
+        <Link to="/contact" className="flat-button">
+        RESUME
+        </Link>
+        </form>
+        <Link to="/projects" className="flat-button">
+            PROJECTS
+        </Link>
+      </div>
+    </div>
+  )
 }
