51bc5d374b1b5e0b981f435b519494fd13cfdcdf
EmbarckleSreerag <sreerag.em@embarckle.com>
2023-01-20 12:10:30 +0530

    Updates

--- Portfolio.f69400ca.js
+++ Portfolio.f69400ca.js
@@ -58679,7 +58679,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37631" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41535" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
